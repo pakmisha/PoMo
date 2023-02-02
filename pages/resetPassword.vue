4fab116a84c936ab484023a8d1cf5d97e3624cf0
Misha <pakmisha9@gmail.com>
2023-02-02 15:57:34 +0600

    add reset password

--- pages/resetPassword.vue
+++ pages/resetPassword.vue
@@ -2,21 +2,32 @@
   <div>
     <div class="section-container section-title-distance">
       <div class="mx-auto mb-10 max-w-[1490px] text-center">
-        <h1 class="heading-big mb-10">Сброс пароля</h1>
-        <p class="plaintext">
-          Введите адрес электронной почты и мы отправим вам ссылку для сброса
-          пароля
-        </p>
+        <h1 class="heading-big mb-10">Восстановление пароля</h1>
+        <p class="plaintext">Введите новый пароль</p>
       </div>
-      <div class="mx-auto max-w-[600px]">
+      <div class="mx-auto max-w-[600px] space-y-5">
         <div class="input-wrapper">
-          <label for="">Адрес электронной почты</label>
-          <input type="email" v-model="email" class="input-primary" />
+          <label for="">Новый пароль</label>
+          <input type="password" v-model="password" class="input-primary" />
           <div
             class="text-error"
-            v-if="errors && errors.hasOwnProperty('email')"
+            v-if="errors && errors.hasOwnProperty('password')"
           >
-            {{ errors.email[0] }}
+            {{ errors.password[0] }}
+          </div>
+        </div>
+        <div class="input-wrapper">
+          <label for="">Повторите пароль</label>
+          <input
+            type="password"
+            v-model="password_confirmation"
+            class="input-primary"
+          />
+          <div
+            class="text-error"
+            v-if="errors && errors.hasOwnProperty('password_confirmation')"
+          >
+            {{ errors.password_confirmation[0] }}
           </div>
         </div>
         <div class="mt-10 flex flex-col items-center justify-center">
@@ -35,7 +46,8 @@
 <script>
 export default {
   data: () => ({
-    email: null,
+    password: null,
+    password_confirmation: null,
     errors: null,
     loading: false,
   }),
@@ -44,10 +56,13 @@ export default {
       this.loading = true;
       this.errors = null;
       try {
-        const response = await this.$axios.post("auth/send-link", {
-          email: this.email,
+        const response = await this.$axios.post("auth/reset-password", {
+          password: this.password,
+          password_confirmation: this.password_confirmation,
+          id: this.$route.query.id,
         });
         this.$toast.success(response.data.message);
+        this.$router.push("/login");
       } catch (e) {
         this.handleValidationErrors(e);
       } finally {
