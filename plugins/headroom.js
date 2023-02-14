a7bd167f087d3e2e9a55a95e77fbd3a7203f1832
Misha <pakmisha9@gmail.com>
2023-02-14 20:00:42 +0600

    edit

--- plugins/headroom.js
+++
@@ -1,9 +0,0 @@
-// import Headroom from "headroom.js";
-// // grab an element
-// const hr = function (el) {
-//   var myElement = document.querySelector("headroom");
-//   var headroom = new Headroom(myElement);
-//   headroom.init();
-// };
-
-// export default hr;
